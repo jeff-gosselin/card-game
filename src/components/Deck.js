0ecfdeba9de21ff5b5adbef2640bc2f1a3773cbc
jeff-gosselin <jgoz.email@gmail.com>
2019-07-23 21:36:05 -0400

    draw card button retrieves single card data

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -17,16 +17,18 @@ class Deck extends Component {
 
 	async getCard() {
 		const id = this.state.deck.deck_id;
-		const drawCard = `${baseURL}/${id}/draw/?count=1/`;
+		console.log("id: ", id);
+		const drawCard = `${baseURL}/${id}/draw/?count=1`;
 		let cardResult = await axios.get(drawCard);
-		console.log(cardResult);
+		let card = cardResult.data.cards[0];
+		console.log("card: ",card);
 	}
 
 	render() {
 		return (
 			<div>
 				<h1>Card Dealer Game</h1>
-				<button onClick={this.getCard}>Draw Card</button>
+				<button onClick={this.getCard.bind(this)}>Draw Card!</button>
 			</div>
 		);
 	}
