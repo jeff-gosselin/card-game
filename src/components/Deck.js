ae99caef647d4f061746ac69a7970cfe2fcf26b5
jeff-gosselin <jgoz.email@gmail.com>
2019-07-23 20:15:01 -0400

    setting up components

---
+++ src/components/Deck.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Deck extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Card Dealer</h1>
+			</div>
+		);
+	}
+}
+
+export default Deck;
