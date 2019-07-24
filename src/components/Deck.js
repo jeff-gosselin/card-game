63b7c3dfc0ebd3212833b4a99e61eee1e43a1088
jeff-gosselin <jgoz.email@gmail.com>
2019-07-23 20:58:33 -0400

    troubleshooting api data retrieval

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const baseURL = "https://deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
+	state = {
+		deck: null
+	}
+
+	async componentDidMount() {
+		let deck = await axios.get(`${baseURL}/new/shuffle/?deck_count=1`);
+		this.setState({
+			deck: deck.data
+		})
+	}
+
+	async getCard() {
+		const id = this.state.deck.deck_id;
+		const drawCard = `${baseURL}/${id}/draw/?count=1/`;
+		let cardResult = await axios.get(drawCard);
+		console.log(cardResult);
+	}
+
 	render() {
 		return (
 			<div>
-				<h1>Card Dealer</h1>
+				<h1>Card Dealer Game</h1>
+				<button onClick={this.getCard}>Draw Card</button>
 			</div>
 		);
 	}
